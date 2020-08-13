5ff54a9f5b5341c9b195d4bce6102f4fd0975cae
rohan-w <rohan.winsor@rap.ventures>
2020-08-13 10:29:42 +0530

    code optimiztion

--- executor.py
+++ executor.py
@@ -4,26 +4,21 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', '-f', help='File name')
-
 args = parser.parse_args()
 
 
-def is_processRunning(file_name, pid):
+def is_processrunning(file_name, pid):
     cmd = [f'pgrep -f .*python3.*{file_name}']
     process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     my_pid, err = process.communicate()
-    my_pids = list(map(int, my_pid.splitlines()))
-    if pid in my_pids:
-        return True
-    else:
-        return False
+    running_pids = list(map(int, my_pid.splitlines()))
+    return pid in running_pids
 
 
 def run_process(file_name, log_name):
-    with open(f'{log_name}.txt', 'a') as f:
-        process = subprocess.Popen(['python3', file_name], stdout=f)
-    return process
+    return subprocess.Popen(['python3', file_name],
+                            stdout=open(f'{log_name}.txt', 'a'))
 
 
 if __name__ == "__main__":
@@ -31,7 +26,7 @@ if __name__ == "__main__":
     process = run_process(filename, f"log_for_{filename}")
     flag = True
     while flag:
-        flag = is_processRunning(filename, pid=process.pid)
+        flag = is_processrunning(filename, pid=process.pid)
         print("Process running !!")
         time.sleep(1)
     print("Process Stopped !!!")
