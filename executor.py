3e5983865656d0e15fc781635b80dc348cca3d0b
rohan-w <rohan.winsor@rap.ventures>
2020-08-18 06:35:09 +0530

    Async functionality added

--- executor.py
+++ executor.py
@@ -6,6 +6,7 @@ import errno
 import time
 import select
 from message import get_message
+import asyncio
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--file', '-f', required=True, help='File name')
@@ -13,8 +14,13 @@ parser.add_argument('--pid', '-p', required=False, help='File name')
 args = parser.parse_args()
 
 
-def is_processrunning(pid):
-    return True if pid.poll() is None else False
+async def async_is_processrunning(process):
+    return True if process.poll() is None else False
+
+
+def is_processrunning(process):
+    return True if process.poll() is None else False
+
 
 def run_process(file_name, log_name):
     return subprocess.Popen(['python3', file_name],
@@ -41,6 +47,23 @@ def reader(IPC_FIFO_NAME, start_time):
     return None
 
 
+async def async_ipc_interface(IPC_FIFO_NAME):
+    try:
+        os.mkfifo(IPC_FIFO_NAME)
+        time.sleep(1)
+    except OSError as oe:
+        if oe.errno == errno.EEXIST:
+            pass
+        else:
+            assert False, f"Error {os.errno}"
+    output = reader(IPC_FIFO_NAME, time.time())
+    if output is None:
+        return None
+    json_values = ast.literal_eval(output)
+    os.remove(IPC_FIFO_NAME)
+    return json_values
+
+
 def ipc_interface(IPC_FIFO_NAME):
     try:
         os.mkfifo(IPC_FIFO_NAME)
@@ -58,12 +81,28 @@ def ipc_interface(IPC_FIFO_NAME):
     return json_values
 
 
+async def main(process, IPC_FIFO_NAME):
+    output = await asyncio.gather(async_is_processrunning(process),
+                                  async_ipc_interface(IPC_FIFO_NAME))
+    return output
+
+
 if __name__ == "__main__":
-    """
-    Asyc process
-    Sub process messages
-    """
     IPC_FIFO_NAME = "TEST"
+    process = run_process(args.file, args.file)
+    flag = True
+    while flag:
+        output = asyncio.run(main(process, IPC_FIFO_NAME))
+        print("Is Process Running ::", output[0])
+        print("Message From process ::", output[1])
+        flag = output[0]
+    os.remove(IPC_FIFO_NAME)
+
+"""
+Sync Running the process
+if __name__ == "__main__":
+    # Asyc process
+    # Sub process messages
     process = run_process(args.file, args.file)
     flag = is_processrunning(process)
     while flag:
@@ -78,3 +117,4 @@ if __name__ == "__main__":
     while output is not None:
         print("Outside--->", output)
     os.remove(IPC_FIFO_NAME)
+"""
